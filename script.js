b4c3c92046f89176b02a62f9fdf62e484461bad1
simoneries <192712243+simoneries@users.noreply.github.com>
2025-06-29 23:39:47 +0200

    Get the data value of buttons

--- script.js
+++ script.js
@@ -36,9 +36,17 @@ const operate = function(num1,num2,operator){
 
 const populateDisplay=function(){
     // Function that updates and store the variables displayed to do the operations
-    // Mechanic 1: You press a button, it displays a number. You can keep doing that until you press on the operator, then it's a call to save the number as variable.
-    const btnContainer = document.querySelector("#buttonContainer")
-    btnContainer.addEventListener("click",()=>console.log("hello"))
+    // Mechanic 1: You press a button, it displays a number. 
+    const btnContainer = document.querySelector("#buttonContainer ")
+    btnContainer.addEventListener("click",function(e){
+        if (e.target.hasAttribute("data-value")){
+            const buttonClicked=e.target
+            console.log(buttonClicked.getAttribute("data-value"))
+        }
+    })
+    //Next step : console log the right value 
+    //next step : display the right numbers
+    //You can keep doing that until you press on the operator, then it's a call to save the number as variable.
     // 1 : You need to pass query selectors to all your buttons. 
     
     // 2 : They need to be able to display the right numbers to the display. 
