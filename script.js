768582a9b7fc087557768448b1f7a9b59e0ba4a8
simoneries <192712243+simoneries@users.noreply.github.com>
2025-06-30 17:23:35 +0200

    Add semicolon everywhere

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
-let num1 = null
+let num1 = null;
 
-let num2 = null
+let num2 = null;
 
-let operator = null
+let operator = null;
 
 const add = function(num1,num2){
     return num1+num2
-}
+};
 
 const substract = function(num1,num2){
     return num1-num2
-}
+};
 
 const multiply =function(num1,num2){
     return num1*num2
-}
+};
 
 const divide = function(num1,num2){
     return num1/num2
-}
+};
 
 const operate = function(num1,num2,operator){
     if (operator === "+"){
@@ -32,24 +32,20 @@ const operate = function(num1,num2,operator){
     } else {
         return "Oops,error"
     }
-}
+};
 
 const populateDisplay=function(){
     // Function that updates and store the variables displayed to do the operations
     // Mechanic 1: You press a button, it displays a number. 
-    const btnContainer = document.querySelector("#buttonContainer ")
-    const display = document.querySelector("#display")
+    const btnContainer = document.querySelector("#buttonContainer ");
+    const display = document.querySelector("#display");
     btnContainer.addEventListener("click",function(e){
         if (e.target.hasAttribute("data-value")){
             const buttonClicked=e.target
             //modify the content of the display - pb here it's only updating ea
-            display.textContent += buttonClicked.getAttribute("data-value")
+            display.textContent += buttonClicked.getAttribute("data-value");
         }
-    })
-    //next step : display a serie of number
-    //You can keep doing that until you press on the operator, then it's a call to save the number as variable.
-    
-    //Mechanic 2 : you clear the display and you do the same. 
+    });
 }
 
  populateDisplay()
