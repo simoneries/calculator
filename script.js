8ac7c26f8a7ebfeb89c61c7b733f3cb2617cd029
simoneries <192712243+simoneries@users.noreply.github.com>
2025-07-01 11:49:30 +0200

    Create a calculatorLogic function

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ let secondNum = null;
 
 let operator = null;
 
-let arr = [];
 
 const add = function(num1,num2){
     return num1+num2
@@ -36,6 +35,31 @@ const operate = function(num1,num2,operator){
     }
 };
 
+const calculatorLogic = function(){
+    
+    const btnContainer = document.querySelector("#buttonContainer");
+    const display = document.querySelector("#display")
+   
+    function populateDisplay(){
+        btnContainer.addEventListener("click",function(e){
+            const btnClicked = e.target;
+            if(btnClicked.hasAttribute("data-value")){
+                display.textContent += btnClicked.getAttribute("data-value")
+            }
+        })
+    };
+    populateDisplay()
+};
+
+calculatorLogic()
+
+//Mechanic : 
+//I click on buttons and it prompts a number on the display
+// I use the click on an operator to store the value and the operator. 
+//The display is cleared
+// the user can continue to enter more number. 
+// if he clicks again on an operator, I hold the temporary result and the operator. They can continue to type in numbers. 
+// when he clicks on =, display the result. 
 
 
 
